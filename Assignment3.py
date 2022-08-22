39fa232c53d9435715e65fc5231ce3961cf413cc
Mohanish-ThatfatKid <107757478+Mohanish-ThatfatKid@users.noreply.github.com>
2022-08-22 14:08:04 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,40 @@
+# Question 1 
+
+x = 10
+str(x)
+
+#Question 2
+print(ord('m'))
+
+#Question 3
+print(chr(100))
+
+#Question 4
+a = 28
+print("Number: ",a,"Binary: ",bin(a))
+
+# Question 5
+
+b = 56
+print("num: ",b ,"Octal: ",oct(b))
+
+# Question 6
+c = 48
+print("Number: ",c , "Hexadecimal: ",hex(c))
+
+# Question 7
+b = 0b1100101
+print(b)
+
+# Question 8
+h = 0x2F
+print(oct(h))
+
+# Question 9
+o = 0o125
+print(o)
+
+# Question 10
+var1 = 0o25
+var2 = 0x39
+print(bin(var1)+bin(var2))

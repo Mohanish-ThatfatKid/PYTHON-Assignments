0987a84b6682f4909bb976b70890b3d1a8fe78d6
Mohanish-ThatfatKid <107757478+Mohanish-ThatfatKid@users.noreply.github.com>
2022-08-22 14:51:11 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,63 @@
+#Question 1
+
+name = input("Enter Your Name: ")
+print(name)
+
+#Question 2
+num = int(input("Enter Any Number: "))
+print(num)
+
+#Question3
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+result = num1+num2
+print(result)
+
+
+# Question 4
+from cmath import pi
+
+
+radius = eval(input("Enter Radius Of Circle: "))
+res = pi*radius**2
+print(res)
+
+# Question 5
+number = int(input("Enter Number to find Square: "))
+sqr = number**2
+print(sqr)
+
+#Question 6
+h = int(input("Enter Height to find Area of Triangle: "))
+b = int(input("Enter base to find Area of triangle: "))
+atri = 1/2*b*h
+print(atri)
+
+# Question 7
+n1 = int(input("Enter first Number: "))
+n2 = int(input("Enter Second Number: "))
+n3 = int(input("Enter Third Number: "))
+
+avg = (n1+n2+n3)/3
+print(avg)
+
+# question 8
+p = 10000
+rate = 5
+tm = 3
+si = (p*rate*tm)/100
+print(si)
+
+
+# Question 9
+w= 12
+hig = 10
+lent = 5
+volcub = w*hig*lent
+print(volcub)
+
+# Question 10
+wr =10
+lr = 10
+areaRect = wr*lr
+print(areaRect)

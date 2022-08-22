71f863a65355bb8b629f2c06250b6d464a3250cc
Mohanish-ThatfatKid <107757478+Mohanish-ThatfatKid@users.noreply.github.com>
2022-08-22 12:38:58 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,39 @@
+# Question 4
+print("Hello Python")
+
+# Question 5
+print("Hello")
+print("Python")
+
+# Question 6
+print("MySirG")
+
+# Question 7
+print("Teacher's Day")
+
+#Question 8
+
+name = "Mohanish"
+print(name)
+
+# Question 9
+
+name_Q9 = "Mohanish"
+age = 27
+qualification = "Graduate"
+yoExperience = 0
+
+print("NAME: ",name_Q9)
+print("Age: ",age)
+print("Qualification: ", qualification)
+print("Experience: ",yoExperience)
+
+
+# Question 10
+num1 = 10
+num2 = 20
+num3 = 30
+
+print(num1)
+print(num2)
+print(num3)
